3bc7bf7fcff07ba9dab705f0177bb35298f720a5
Chun Kai <CK-SmartScreen@users.noreply.github.com>
2017-12-27 23:12:49 +1300

    add Shopping_Cart and CartItem to models

--- product_app/admin.py
+++ product_app/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from .models import Supplier, Category, Hat
+from .models import Supplier, Category, Hat, ShoppingCart, CartItem
 
 # Register your models here.
 admin.site.register(Supplier)
 admin.site.register(Category)
 admin.site.register(Hat)
+admin.site.register(ShoppingCart)
+admin.site.register(CartItem)
--- product_app/models.py
+++ product_app/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 
 
@@ -24,8 +24,27 @@ class Hat(models.Model):
     description = models.CharField(max_length=265)
     price = models.DecimalField(max_digits=8, decimal_places=2)
     image = models.ImageField(upload_to='hat_pics')
-    supplier = models.ForeignKey(Supplier, on_delete=models.CASCADE)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    supplier = models.ForeignKey(Supplier, blank=True, null=True, on_delete=models.SET_NULL, related_name='suppliers')
+    category = models.ForeignKey(Category, blank=True, null=True, on_delete=models.SET_NULL, related_name='categories')
 
     def __str__(self):
         return self.name
+
+
+class ShoppingCart(models.Model):
+    quantity = models.PositiveIntegerField(default=0)
+    GST = models.DecimalField(decimal_places=2, max_digits=8)
+    total_price = models.DecimalField(decimal_places=2, max_digits=10)
+    client = models.ForeignKey(User, blank=True, null=True, on_delete=models.SET_NULL)
+
+    def __str__(self):
+        return str(self.client.username)
+
+class CartItem(models.Model):
+    product = models.ForeignKey(Hat, on_delete=None)
+    price = models.DecimalField(decimal_places=2, max_digits=8)
+    quantity = models.PositiveIntegerField(default=0)
+    cart = models.ForeignKey(ShoppingCart, blank=True, null=True, on_delete=models.SET_NULL)
+
+    def __str__(self):
+        return self.product.name
